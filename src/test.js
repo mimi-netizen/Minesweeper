e1466a6ae132dbdb73b4e20fe25e132ecb600c1f
Celyne <dianakydd2@gmail.com>
2024-03-03 10:33:50 +0300

    Created typescript code

---
+++ src/test.js
@@ -0,0 +1,9 @@
+var a;
+var b;
+var c;
+var user = {
+    name: "Celyne",
+    surname: "Kydd",
+    age: 30,
+    isAdmin: true,
+};
---
+++ src/test.ts
@@ -0,0 +1,17 @@
+let a: number;
+let b: boolean;
+let c: string;
+
+type User = {
+  name: string;
+  surname: string;
+  age: number;
+  isAdmin: boolean;
+};
+
+const user: User = {
+  name: "Celyne",
+  surname: "Kydd",
+  age: 30,
+  isAdmin: true,
+};
